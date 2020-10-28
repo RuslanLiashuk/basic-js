d471bf20725d7cab5459617f7bcc2d8d07f6165e
RuslanLiashuk <70751507+RuslanLiashuk@users.noreply.github.com>
2020-10-29 00:48:10 +0300

    dosc: update file

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (typeof(members) !== 'object'  || members === undefined || members === null) return false;
+  let NAMES = '';
+  for (let i = 0; i < members.length; i++) { 
+    if (typeof(members[i]) === 'string') { 
+    NAMES += members[i].trim().toUpperCase().split('')[0];
+  }
+}
+  return NAMES.split('').sort().join('');
 };
